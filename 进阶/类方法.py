586aa5f7892c5ef81f8713ba3a1d410566c6d5d5
yr <1350181508@qq.com>
2024-11-08 12:29:32 +0800

    类方法

---
+++ 进阶/类方法.py
@@ -0,0 +1,43 @@
+class Dog:  ## 类名首字母一般大写
+    # 类属性
+    dogs = []  # 保存每只狗
+
+    # 类方法
+    @classmethod
+    def num_of_dogs(cls):
+        return len(cls.dogs)
+    @classmethod
+    def biggest_dog(cls):
+        max_height = -5
+        for dog in cls.dogs:
+            if dog.height > max_height:
+                max_height = dog.height
+        return max_height
+
+    # 构造方法
+    # 此处self可以理解为类的实例化的地址，如d1 = Dog()，d1即为一个实例，self指向实例地址
+    def __init__(self, name, height, power):
+        self.name = name
+        self.height = height
+        self.blood = 100  # 有的属性可以在模版直接创建
+        self.power = power
+        Dog.dogs.append(self)
+
+
+    def bark(self):
+        print(f'My name is {self.name},My blood is {self.blood}')
+    def attack(self,enemy):
+        enemy.reduce_blood(self.power)
+    def reduce_blood(self,val):
+        if self.blood > val:
+            self.blood -= val
+        else:
+            self.blood = 0
+
+# 创建实例
+d1 = Dog(name="大黄",height=1.2,power=20)
+d2 = Dog(name="小黑",height=1.5,power=30)
+
+print(Dog.num_of_dogs())
+print(Dog.biggest_dog())
+
